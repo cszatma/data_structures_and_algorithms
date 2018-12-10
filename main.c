9fe9d9d28a3356517ba076a8f5fadf0514aeb9e6
Christopher Szatmary <cs@christopherszatmary.com>
2018-12-10 11:40:34 -0500

    Ran unit tests in main.c

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include "tests/linked_list_test.h"
+#include "tests/list_stack_test.h"
+#include "tests/array_stack_test.h"
 
 int main() {
+    run_linked_list_tests();
+    run_list_stack_tests();
+    run_array_stack_tests();
+
     return 0;
 }
